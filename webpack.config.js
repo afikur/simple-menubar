496c8526ca3f1ef7daa3a5b65d17ac06451fc978
Afikur Rahman <afikur.khan@dsinnovators.com>
2019-10-09 22:27:38 +0600

    add menubar

--- webpack.config.js
+++ webpack.config.js
@@ -19,5 +19,18 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
